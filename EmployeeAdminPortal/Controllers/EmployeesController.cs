4191503b54a1e5500d3b64c120b7caec4d90ff4a
Salman Aslam <Salman.Aslam@Stewart.com>
2024-06-05 19:02:14 +0500

    read function added

---
+++ EmployeeAdminPortal/Controllers/EmployeesController.cs
@@ -0,0 +1,25 @@
+﻿using EmployeeAdminPortal.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAdminPortal.Controllers
+{
+    // localhost:xxxx/api/employees
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public EmployeesController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            return Ok(dbContext.Employees.ToList());
+        }
+    }
+}
