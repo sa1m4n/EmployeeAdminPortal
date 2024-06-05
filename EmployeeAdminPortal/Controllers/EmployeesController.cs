a8dd8f8088e6eabb045a51b26548ed79973c5b79
Salman Aslam <Salman.Aslam@Stewart.com>
2024-06-05 20:28:34 +0500

    delete operation added

--- EmployeeAdminPortal/Controllers/EmployeesController.cs
+++ EmployeeAdminPortal/Controllers/EmployeesController.cs
@@ -78,5 +78,23 @@ namespace EmployeeAdminPortal.Controllers
                 return Ok(employee);
             }
         }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public IActionResult DeleteEmployee(Guid id)
+        {
+            var employee = dbContext.Employees.Find(id);
+
+            if (employee is null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                dbContext.Employees.Remove(employee);
+                dbContext.SaveChanges();
+                return Ok();
+            }
+        }
     }
 }
